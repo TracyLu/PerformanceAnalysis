322dbc45a4664192d9c68f52303e953545d19db2
TracyLu <hailinglu@126.com>
2014-08-10 17:44:14 +0800

    Refactor PerformanceDataUI

--- src/main/scala/net/imadz/performance/PerformanceDataUI.scala
+++ src/main/scala/net/imadz/performance/PerformanceDataUI.scala
@@ -13,30 +13,30 @@ import net.imadz.performance.metadata.{DiskIO, SourceMetadata}
  */
 object PerformanceDataUI extends SimpleSwingApplication {
 
-  val perfData: List[(String, List[Double])] = {
+  def perfData(col: Int): List[(String, List[Double])] = {
 
-    val firstdata = Source.fromFile("testdata/handled/autoincremental_io.log").getLines().drop(1).map { line =>
-      parseDouble(line.split("\t")(7)).toOption.get
+    val firstdata = Source.fromFile("/Users/Scala/Workspaces/multitenant-uuid/report/20140810052524/Query_a˜gainst_AutoIncremental_PK/io.log.updated").getLines().drop(1).map { line =>
+      parseDouble(line.split("\t")(col)).toOption.get
     }.toList
 
-    val seconddata = Source.fromFile("testdata/handled/binary_io.log").getLines().drop(1).map { line =>
-      parseDouble(line.split("\t")(8)).toOption.get
+    val seconddata = Source.fromFile("/Users/Scala/Workspaces/multitenant-uuid/report/20140810052337/Query_against_Binary_UUID/io.log.updated").getLines().drop(1).map { line =>
+      parseDouble(line.split("\t")(col)).toOption.get
     }.toList
 
-    val thirddata = Source.fromFile("testdata/handled/binary_io.log").getLines().drop(1).map { line =>
-      parseDouble(line.split("\t")(9)).toOption.get
-    }.toList
-    List(("autoincremental", firstdata),("binary", seconddata), ("hex", thirddata))
+//    val thirddata = Source.fromFile("testdata/handled/binary_io.log").getLines().drop(1).map { line =>
+//      parseDouble(line.split("\t")(9)).toOption.get
+//    }.toList
+    List(("autoincremental", firstdata),("binary", seconddata))
   }
 
-  val sourceMetadata = new SourceMetadata(DiskIO(), "queesize")
+  def sourceMetadata(dimensionName: String) = new SourceMetadata(DiskIO(), dimensionName)
 
   def top = new MainFrame {
     this.preferredSize = Toolkit.getDefaultToolkit().getScreenSize();
     contents = new BoxPanel(Orientation.Vertical) {
-      contents += scala.swing.Component.wrap(new PGraph(sourceMetadata,perfData))
-      contents += scala.swing.Component.wrap(new PGraph(sourceMetadata,perfData))
-      contents += scala.swing.Component.wrap(new PGraph(sourceMetadata,perfData))
+      contents += scala.swing.Component.wrap(new PGraph(sourceMetadata("r/s"),perfData(3)))
+      contents += scala.swing.Component.wrap(new PGraph(sourceMetadata("avgqu-sz"),perfData(8)))
+      contents += scala.swing.Component.wrap(new PGraph(sourceMetadata("%util"),perfData(13)))
     }
   }
 
